51d1d51b727dbcb0766905459c6a839922903eca
Harry Dibden <harry-dibden@hotmail.com>
2018-07-16 15:49:42 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,8 +7,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(move)
-  move.to_i - 1
+def input_to_index(choice)
+  choice.to_i - 1
 end
   
 def move(array, index, value = "X")
